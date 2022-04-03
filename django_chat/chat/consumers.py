b3069d9082e0d437634e72b4376c9234b49be230
adrian-xps-ubuntu <adriandanao@gmail.com>
2022-04-03 13:31:58 -0400

    setup routing

---
+++ django_chat/chat/consumers.py
@@ -0,0 +1,16 @@
+import json
+from channels.generic.websocket import WebsocketConsumer
+
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+
+        self.send(
+            text_data=json.dumps(
+                {
+                    "type": "connection_established",
+                    "message": "you are now connecte!",
+                }
+            )
+        )
--- django_chat/django_chat/asgi.py
+++ django_chat/django_chat/asgi.py
@@ -10,8 +10,18 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_chat.settings")
 
-application = ProtocolTypeRouter({"http": get_asgi_application()})
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(chat.routing.websocket_urlpatterns)
+        ),
+    }
+)
