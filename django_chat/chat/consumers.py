2f06306d48c48a7c45f5a5ef3fe25ef9f6313891
adrian-xps-ubuntu <adriandanao@gmail.com>
2022-04-03 14:34:42 -0400

    message sending

--- django_chat/chat/consumers.py
+++ django_chat/chat/consumers.py
@@ -14,3 +14,11 @@ class ChatConsumer(WebsocketConsumer):
                 }
             )
         )
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+
+        print("Message:\t", message)
+
+        self.send(text_data=json.dumps({"type": "chat", "message": message}))
