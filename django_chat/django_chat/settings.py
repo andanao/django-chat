864d218603f73d2e7943bafd92d9b28e04c861b9
adrian-xps-ubuntu <adriandanao@gmail.com>
2022-04-03 14:49:30 -0400

    messaging working

--- django_chat/django_chat/settings.py
+++ django_chat/django_chat/settings.py
@@ -45,6 +45,10 @@ INSTALLED_APPS = [
 
 ASGI_APPLICATION = "django_chat.asgi.application"
 
+CHANNEL_LAYERS = {
+    "default": {"BACKEND": "channels.layers.InMemoryChannelLayer"}
+}
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
