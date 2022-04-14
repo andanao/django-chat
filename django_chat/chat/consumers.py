00e9e0128affda48247db22f622d41d2794e8a99
adrian-xps-ubuntu <adriandanao@gmail.com>
2022-04-13 21:46:41 -0400

    async websocket consumer

--- django_chat/chat/consumers.py
+++ django_chat/chat/consumers.py
@@ -1,17 +1,19 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
-from asgiref.sync import async_to_sync
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.room_group_name = "test"
 
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
+        # async_to_sync(self.channel_layer.group_add)(
+        #     self.room_group_name, self.channel_name
+        # )
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
         )
-
-        self.accept()
+        await self.accept()
 
         # self.send(
         #     text_data=json.dumps(
@@ -22,18 +24,21 @@ class ChatConsumer(WebsocketConsumer):
         #     )
         # )
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
         # print("Message:\t", message)
 
         # self.send(text_data=json.dumps({"type": "chat", "message": message}))
-        async_to_sync(self.channel_layer.group_send)(
+        # async_to_sync(self.channel_layer.group_send)(
+        #     self.room_group_name, {"type": "chat_message", "message": message}
+        # )
+        await self.channel_layer.group_send(
             self.room_group_name, {"type": "chat_message", "message": message}
         )
 
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event["message"]
 
-        self.send(text_data=json.dumps({"type": "chat", "message": message}))
+        await self.send(text_data=json.dumps({"type": "chat", "message": message}))
